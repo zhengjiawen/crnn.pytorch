9f11b3e69e3de0e246ebb2dfc84777a86a85bba6
zhengjiawen <zhengjiawen77@gmail.com>
2019-07-27 16:33:02 +0800

    update train sh

--- train_baseline.sh
+++ train_baseline.sh
@@ -1,15 +1,15 @@
-GPU=1,2
+GPU=3
 CUDA_VISIBLE_DEVICES=${GPU} \
 python train.py \
 	--trainRoot /data/home/zjw/dataset/AI+_Chinese_Scene_Text/lmdb/ \
 	--workers 2 \
-	--batchSize 130 \
+	--batchSize 64 \
 	--nepoch 100 \
 	--lr 0.1 \
 	--expr_dir output/baselineep100_lr0.1_rlop/ \
-	--displayInterval 100 \
+	--displayInterval 200 \
 	--adadelta  \
 	--alphabet ./CST_alphabet.txt \
 	--random_sample \
 	--adam \
-	--valInterval 800
+	--valInterval 1000
